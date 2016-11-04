caa7c92eba217de8dd72c68b665fc82f2a060c7f
Łukasz Hejnak (LeHack) <lehack-ghub@lehack.pl>
2016-11-04 21:50:59 +0100

    Simple digit display (<=99)

--- src/TestMatrix.vhd
+++ src/TestMatrix.vhd
@@ -2,6 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.machine_state_type.all;
+use work.fonts.all;
 
 entity TestMatrix is
     PORT (
@@ -14,19 +15,8 @@ END entity;
 architecture main of TestMatrix is
     signal state            : machine_state_type := initialize; -- RO here
     signal virt_clk, run    : STD_LOGIC := '0';
-    signal data             : STD_LOGIC_VECTOR(31 downto 0);
+    signal data             : STD_LOGIC_VECTOR(15 downto 0);
     shared variable sleep   : unsigned(24 downto 0) := (others => '0');
-    type image_type is array (0 to 7) of std_logic_vector(7 downto 0);
-    constant image_data : image_type := (
-        "00000001",
-        "00000011",
-        "00000110",
-        "00000110",
-        "11001100",
-        "01101100",
-        "00111000",
-        "00011000"
-    );
 begin
     led_matrix : entity work.led_matrix PORT MAP (
         CLOCK_50 => CLOCK_50, DIN => DIN, CS => CS, CLK => CLK,
@@ -34,45 +24,33 @@ begin
     );
 
     process(virt_clk, state)
-        variable blink      : boolean := false;
-        variable addr       : unsigned(3 downto 0);
-        type PWMwidthType is array(1 downto 0) of unsigned(6 downto 0);
-        variable PWM_width  : PWMwidthType;
-        variable PWM_adj    : unsigned(5 downto 0);
-        variable cnt        : unsigned(11 downto 0);
+        variable count  : unsigned(5 downto 0) := (others => '0');
+        variable addr   : unsigned(3 downto 0) := (0 => '1', others => '0');
+        variable tens, single : integer := 0;
         procedure run_animation is
-            constant firstbit   : unsigned(6 downto 0)  := (others => '0');
-            variable dat1, dat2 : std_logic_vector(15 downto 0);
         begin
-            -- we've got 8 rows
             if addr <= 8 then
-                -- refresh display
-                dat1 := ( 8 => addr(0), 9 => addr(1), 10 => addr(2), 11 => addr(3), others => '0' );
-                dat2 := dat1;
-                for I in 0 to 7 loop
-                    if image_data(to_integer(addr)-1)(I) = '1' then
-                        dat1(I) := PWM_width(0)(6);
-                        dat2(I) := PWM_width(1)(6);
-                    end if;
+                -- first set the address
+                data <= "0000" & addr(3) & addr(2) & addr(1) & addr(0) & "00000000";
+                if tens > 0 then
+                    for I in 0 to 3 loop
+                        data(I+4) <= digits(tens)(to_integer(addr)-1)(I);
+                    end loop;
+                end if;
+                for I in 0 to 3 loop
+                    data(I) <= digits(single)(to_integer(addr)-1)(I);
                 end loop;
-                data <= dat1 & dat2;
                 run <= '1';
-                addr := addr + 1;  -- next row
+                addr := addr + 1;
             else
-                -- reset the address to start row
-                addr := (0 => '1', others => '0');
-                cnt  := cnt + 1;
-                PWM_width(0) := '0' & PWM_width(0)(5 downto 0) + PWM_adj;
-                PWM_width(1) := '0' & PWM_width(1)(5 downto 0) + not PWM_adj;
-                if blink then
-                    PWM_adj := cnt(10 downto 5);
-                else
-                    PWM_adj := not cnt(10 downto 5);
-                end if;
-                if cnt(11) = '1' then
-                    cnt := (10 => '1', others => '0');
-                    blink := not blink; -- toggle led state for each row
-                end if;
+                -- update addr/set sleep time
+                addr  := ( 0 => '1', others => '0');
+                sleep := (13 => '1', others => '0');
+                -- increase count
+                count := count + 1;
+                -- now also set the digits
+                tens   := to_integer(count) / 10;
+                single := to_integer(count) mod 10;
             end if;
         end procedure run_animation;
     begin
--- src/led_matrix.vhd
+++ src/led_matrix.vhd
@@ -6,7 +6,7 @@ use work.machine_state_type.all;
 entity led_matrix is
     PORT (
         CLOCK_50        : IN STD_LOGIC;
-        input           : IN STD_LOGIC_VECTOR(31 downto 0);
+        input           : IN STD_LOGIC_VECTOR(15 downto 0);
         run             : IN STD_LOGIC := '0';
         sleep           : IN unsigned(24 downto 0) := (others => '0');
         state           : BUFFER machine_state_type := initialize;
@@ -16,10 +16,10 @@ entity led_matrix is
 END entity;
 
 architecture max7219 of led_matrix is
-    signal data         : STD_LOGIC_VECTOR(31 downto 0);
+    signal data         : STD_LOGIC_VECTOR(15 downto 0);
     signal enable, sclk : STD_LOGIC := '0';
 begin
-    spi : entity work.spi_master GENERIC MAP (slaves => 1, d_width => 32) PORT MAP (
+    spi : entity work.spi_master GENERIC MAP (slaves => 1, d_width => 16) PORT MAP (
         clock => sclk, enable => enable, busy => CS, cont => '0',
         reset_n => '1', cpol => '0', cpha => '0', addr => 0,
         tx_data => data, miso => 'Z', mosi => DIN, sclk => CLK, clk_div => 0
@@ -65,7 +65,7 @@ begin
             (1 => '1', 9 => '1', 11 => '1', others => '0'),
             -- disable display test
             (8 => '1', 9 => '1', 10 => '1', 11 => '1', others => '0'),
-            -- enable scan to 2 digits
+            -- enable scan for digit '0'
             (0 => '1', 1 => '1', 2 => '1', 8 => '1', 9 => '1', 11 => '1', others => '0'),
             -- disable decode
             (8 => '1', 11 => '1', others => '0'),
@@ -80,10 +80,9 @@ begin
             (8 => '0', 9 => '0', 10 => '0', 11 => '1', others => '0')
         );
         variable setup_step : integer range 0 to setup_step_cnt-1 := 0;
-        variable display_no : integer range 0 to 1 := 0;
         procedure run_setup is
         begin
-            data <= setup_steps(setup_step) & setup_steps(setup_step);
+            data <= setup_steps(setup_step);
             setup_step := setup_step + 1;
             if setup_step = 0 then
                 state <= ready;
